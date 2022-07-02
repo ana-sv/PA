857d0dfe2fd73cb75ac975bad2e11fc1d957e741
anarita-sv <anarita.svideira@gmail.com>
2022-07-02 17:13:37 +0100

    e17

---
+++ src/exercises/e17/IInventario.java
@@ -0,0 +1,10 @@
+package exercises.e17;
+
+
+public interface IInventario {
+    public void addProduto( String designacao, int codigo, int quantidade); 
+    public float getPreco ( int codigo ); 
+    public float getPreco ( String designacao );
+    public int  getCodigo ( String designacao ); 
+    public String getNomes(); 
+}
